aa3afda719cee25094445d6a5f1349b513e0f569
Praphul Kumar <praphulkumar2023@gmail.com>
2024-07-12 10:45:34 +0530

    Using .env in Node JS

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
+// Serving Static Files
+// It will basically server static files in browser if there is no route defined with the serched URL then it will look into the given static files directory if there will be a file avialable then it will return that file 
+app.use(express.static(`${__dirname}/public`));
+
 // Routes
-app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
 });
