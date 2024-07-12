fac814bc8659a170f7c1e133505cc1aa7941d3c4
Praphul Kumar <praphulkumar2023@gmail.com>
2024-07-12 11:13:29 +0530

    Configured EsLint & Prettier

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
 // Serving Static Files
-// It will basically server static files in browser if there is no route defined with the serched URL then it will look into the given static files directory if there will be a file avialable then it will return that file 
+// It will basically server static files in browser if there is no route defined with the serched URL then it will look into the given static files directory if there will be a file avialable then it will return that file
 app.use(express.static(`${__dirname}/public`));
 
 // Routes
