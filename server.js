f1ebff3c655bfff24ce6a3bc42978aa8aca1cd11
Praphul Kumar <praphulkumar2023@gmail.com>
2024-07-25 11:07:43 +0530

    MongoDb: First CRUD

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'Tour must have a name'],
+    required: [true, 'Tour must have a name'],
     unique: true,
   },
   rating: {
@@ -31,11 +31,26 @@ const tourSchema = new mongoose.Schema({
   },
   price: {
     type: Number,
-    require: [true, 'Tour must have a price'],
+    required: [true, 'Tour must have a price'],
   },
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.8,
+  price: 597,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(`Error Occured Saving Tour: ${err}`);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
